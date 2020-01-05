e79ed5d5bef6b098c0db6246b17bbe7b8477730f
MibenCoop <mindkoop@gmail.com>
2020-01-05 15:02:44 +0300

    Added fucntionality of showing city. And some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <ul className="menu">
+            <li className="routerLink">
+              <Link to="/moscow">Moscow</Link>
+            </li>
+            <li className="routerLink">
+              <Link to="/saint-peterburg">Saint-peterburg</Link>
+            </li>
+            <li className="routerLink">
+              <Link to="/rostov-na-dony">Rostov-na-dony</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/moscow">
+            <City currentCity="moscow" />
+          </Route>
+          <Route path="/saint-peterburg">
+            <City currentCity="saint-peterburg" />
+          </Route>
+          <Route path="/rostov-na-dony">
+            <City currentCity="mosrostov-na-dony" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
+};
+
+function City({ currentCity }: any) {
+  console.log("name", currentCity);
+  return <p>{currentCity}</p>;
 }
 
 export default App;
